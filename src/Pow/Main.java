b50bc006723cf0e42a2d89b236dac2fcb9fb55da
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2020-08-08 13:54:23 +0300

    1 version

---
+++ src/Pow/Main.java
@@ -0,0 +1,8 @@
+package Pow;
+
+public class Main {
+    public static void main(String[] args) {
+        Metod m = new Metod();
+        System.out.println(m.pow(23,-2));
+    }
+}
---
+++ src/Pow/Metod.java
@@ -0,0 +1,28 @@
+package Pow;
+
+public class Metod {
+    public double pow  (double powValue, double numb){
+        double result=1;
+//        for (int i = 1; i <= powValue; i++) {
+//            result = result * numb;
+//        }
+
+        if (powValue%2==0){
+            double avrPowValue = powValue/2;
+            for (int i = 1; i <= avrPowValue; i++) {
+                result = result * numb;
+            }
+            return result*result;
+        }
+        else
+        {
+            double avrPowValue = (powValue-1)/2;
+            for (int i = 1; i <= avrPowValue; i++) {
+                result = result * numb;
+            }
+            return result*result*2;
+        }
+
+//        return result;
+    }
+}
